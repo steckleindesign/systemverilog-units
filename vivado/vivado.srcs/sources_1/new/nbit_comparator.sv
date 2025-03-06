e3d5a885833caa6a169793fe3cd55b4dd88d7b30
steckleindesign <steckleindesign@gmail.com>
2025-03-05 23:58:11 -0500

    N-bit comparator that supports signed and unsigned inputs

---
+++ vivado/vivado.srcs/sources_1/new/nbit_comparator.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////
+
+module nbit_comparator
+#(
+    parameter
+    int N           = 8,
+    bit SIGNED_MODE = 0 // 0 = Unsigned, 1 = Signed
+)(
+    input  logic [N-1:0] A,
+    input  logic [N-1:0] B,
+    output logic eq,
+    output logic lt,
+    output logic gt
+);
+
+    generate
+        if (SIGNED_MODE) begin : signed_comparator
+            assign eq = ($signed(A) == $signed(B));
+            assign lt = ($signed(A) <  $signed(B));
+            assign gt = ($signed(A) >  $signed(B));
+        end else begin : unsigned_comparator
+            assign eq = (A == B);
+            assign lt = (A < B);
+            assign gt = (A > B);
+        end
+    endgenerate
+
+endmodule
