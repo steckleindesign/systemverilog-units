d62bbf5af3f2ce85d9a361007324061bc9f2560b
steckleindesign <steckleindesign@gmail.com>
2025-04-02 23:57:47 -0400

    adding features to generic burst fifo

--- vivado/vivado.srcs/sources_1/new/burst_fifo_nx.sv
+++ vivado/vivado.srcs/sources_1/new/burst_fifo_nx.sv
@@ -16,23 +16,37 @@ module burst_fifo_nx #(
     output logic empty, full, almost_empty, almost_full
 );
 
+    logic fifo_empty, fifo_full, fifo_almost_empty, fifo_almost_full;
+
+    // FIFO data with parameterizable width and depth
     logic [WIDTH-1:0] fifo_mem [0:DEPTH-1];
     
-    logic [$clog2(DEPTH)-1:0] wr_ptr, rd_ptr;
-    
     // Cross clock domains with pointers gray encoded
+    logic [$clog2(DEPTH)-1:0] wr_ptr_bin, rd_ptr_bin;
+    logic [$clog2(DEPTH)-1:0] rd_ptr_gray, wr_ptr_gray;
+    logic [$clog2(DEPTH)-1:0] rd_ptr_wrclk, rd_ptr_wrclk1, rd_ptr_wrclk2;
+    logic [$clog2(DEPTH)-1:0] wr_ptr_rdclk, wr_ptr_rdclk1, wr_ptr_rdclk2;
     
-    logic rd_ptr_wrclk, rd_ptr_wrclk1, rd_ptr_wrclk2;
-    logic wr_ptr_rdclk, wr_ptr_rdclk1, wr_ptr_rdclk2;
+    function automatic logic [$clog2(DEPTH)-1:0] bin2gray(input logic [$clog2(DEPTH)-1:0] bin);
+        return bin ^ (bin >> 1);
+    endfunction
+    
+    // Practice coding this in different ways
+    function automatic logic [$clog2(DEPTH)-1:0] gray2bin(input logic [$clog2(DEPTH)-1:0] gray);
+        logic [$clog2(DEPTH)-1:0] bin;
+        for (int i = $clog2(DEPTH); i >= 0; i--)
+            bin[i] = ^(gray >> i);
+        return bin;
+    endfunction
     
     always_ff @(wr_clk) begin
-        rd_ptr_wrclk2 <= rd_ptr;
+        rd_ptr_wrclk2 <= rd_ptr_bin;
         rd_ptr_wrclk1 <= rd_ptr_wrclk2;
         rd_ptr_wrclk  <= rd_ptr_wrclk1;
     end
     
     always_ff @(rd_clk) begin
-        wr_ptr_rdclk2 <= wr_ptr;
+        wr_ptr_rdclk2 <= wr_ptr_bin;
         wr_ptr_rdclk1 <= wr_ptr_rdclk2;
         wr_ptr_rdclk  <= wr_ptr_rdclk1;
     end
@@ -41,11 +55,14 @@ module burst_fifo_nx #(
         case(FASTER_CLK)
             "wr": begin
                 always_ff @(posedge wr_clk) begin
-                
+                    
                 end
                 
                 always_ff @(posedge rd_clk) begin
-                
+                    if (rd_en && ~fifo_empty) begin
+                        rd_ptr_bin++;
+                        rd_ptr_gray <= bin2gray(rd_ptr_bin++);
+                    end
                 end
             end
             "rd": begin
