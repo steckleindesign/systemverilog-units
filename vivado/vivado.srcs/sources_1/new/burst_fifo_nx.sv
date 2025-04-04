9127ae1fe3fcbdae0b71197a5d758e87d1e9f927
steckleindesign <steckleindesign@gmail.com>
2025-04-03 22:34:19 -0400

    working through generic async burst fifo

--- vivado/vivado.srcs/sources_1/new/burst_fifo_nx.sv
+++ vivado/vivado.srcs/sources_1/new/burst_fifo_nx.sv
@@ -12,7 +12,7 @@ module burst_fifo_nx #(
     input  logic wr_clk, rd_clk,
     input  logic wr_en,  rd_en,
     input  logic [WIDTH-1:0] din,
-    output logic [WIDTH-1:0]dout,
+    output logic [WIDTH-1:0] dout,
     output logic empty, full, almost_empty, almost_full
 );
 
@@ -40,48 +40,79 @@ module burst_fifo_nx #(
     endfunction
     
     always_ff @(wr_clk) begin
-        rd_ptr_wrclk2 <= rd_ptr_bin;
+        rd_ptr_wrclk2 <= rd_ptr_gray;
         rd_ptr_wrclk1 <= rd_ptr_wrclk2;
-        rd_ptr_wrclk  <= rd_ptr_wrclk1;
+        rd_ptr_wrclk  <= gray2bin(rd_ptr_wrclk1);
     end
     
     always_ff @(rd_clk) begin
-        wr_ptr_rdclk2 <= wr_ptr_bin;
+        wr_ptr_rdclk2 <= wr_ptr_gray;
         wr_ptr_rdclk1 <= wr_ptr_rdclk2;
-        wr_ptr_rdclk  <= wr_ptr_rdclk1;
+        wr_ptr_rdclk  <= gray2bin(wr_ptr_rdclk1);
+    end
+    
+    always_ff @(rd_clk) begin
+        fifo_empty        <=  rd_ptr_bin    == wr_ptr_rdclk;
+        fifo_almost_empty <= (rd_ptr_bin+1) == wr_ptr_rdclk;
+    end
+    
+    always_ff @(wr_clk) begin
+        fifo_full        <=  wr_ptr_bin    == rd_ptr_wrclk;
+        fifo_almost_full <= (wr_ptr_bin+1) == rd_ptr_wrclk;
     end
     
     generate
         case(FASTER_CLK)
             "wr": begin
                 always_ff @(posedge wr_clk) begin
-                    
+                    if (wr_en && ~fifo_full) begin
+                        wr_ptr_bin++;
+                        wr_ptr_gray <= bin2gray(wr_ptr_bin+1);
+                        fifo_mem[wr_ptr_bin] <= din;
+                    end
                 end
                 
                 always_ff @(posedge rd_clk) begin
                     if (rd_en && ~fifo_empty) begin
                         rd_ptr_bin++;
-                        rd_ptr_gray <= bin2gray(rd_ptr_bin++);
+                        rd_ptr_gray <= bin2gray(rd_ptr_bin+1);
+                        dout <= fifo_mem[rd_ptr_bin];
                     end
                 end
             end
             "rd": begin
                 always_ff @(posedge wr_clk) begin
-                
+                    if (wr_en && ~fifo_full) begin
+                        wr_ptr_bin++;
+                        wr_ptr_gray <= bin2gray(wr_ptr_bin+1);
+                        fifo_mem[wr_ptr_bin] <= din;
+                    end
                 end
                 
                 always_ff @(posedge rd_clk) begin
-                
+                    if (rd_en && ~fifo_empty) begin
+                        rd_ptr_bin++;
+                        rd_ptr_gray <= bin2gray(rd_ptr_bin+1);
+                        dout <= fifo_mem[rd_ptr_bin];
+                    end
                 end
             end
-            // wr clk faster is default
+            // wr clk is same frequency as read clock
             default: begin
                 always_ff @(posedge wr_clk) begin
-                
+                    if (wr_en && ~fifo_full) begin
+                        wr_ptr_bin++;
+                        wr_ptr_gray <= bin2gray(wr_ptr_bin+1);
+                        fifo_mem[wr_ptr_bin] <= din;
+                    end
                 end
                 
                 always_ff @(posedge rd_clk) begin
-                
+                    if (rd_en && ~fifo_empty) begin
+                        rd_ptr_bin++;
+                        rd_ptr_gray <= bin2gray(rd_ptr_bin+1);
+                        dout <= fifo_mem[rd_ptr_bin];
+                    end
                 end
             end
         endcase
